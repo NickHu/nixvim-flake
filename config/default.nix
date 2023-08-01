7a0783889c3a03af608e0f7204085f426386f859
Nick Hu <me@nickhu.co.uk>
2023-08-01 15:59:53 +0100

    nvim-cmp: init

--- config/default.nix
+++ config/default.nix
@@ -31,6 +31,56 @@
         enable = true;
       };
       markdown-preview.enable = true;
+      nvim-cmp = {
+        enable = true;
+        mappingPresets = [ "insert" "cmdline" ];
+        mapping = {
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            modes = [ "i" "s" ];
+            action = ''
+              function(fallback)
+                local has_words_before = function()
+                  unpack = unpack or table.unpack
+                  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+                end
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif has_words_before() then
+                  cmp.complete()
+                else
+                  fallback()
+                end
+              end
+            '';
+          };
+          "<S-Tab>" = {
+            modes = [ "i" "s" ];
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                else
+                  fallback()
+                end
+              end
+            '';
+          };
+        };
+        sources = [
+          { name = "nvim_lsp"; groupIndex = 1; }
+          { name = "nvim_lsp_signature_help"; groupIndex = 1; }
+          { name = "calc"; groupIndex = 1; }
+          { name = "path"; groupIndex = 1; }
+          { name = "buffer"; groupIndex = 2; }
+          { name = "cmp_pandoc"; groupIndex = 1; }
+          { name = "spell"; groupIndex = 1; }
+          { name = "latex_symbols"; groupIndex = 1; }
+        ];
+      };
     };
   };
 }
