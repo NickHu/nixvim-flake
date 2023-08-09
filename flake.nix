25462e49f552c6130cda1f5ed024152639ebfb9d
Nick Hu <me@nickhu.co.uk>
2023-08-09 14:36:30 +0100

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -3,37 +3,40 @@
 
   inputs = {
     nixvim.url = "github:nix-community/nixvim";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "nixvim/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    nixpkgs,
-    nixvim,
-    flake-utils,
-    ...
-  } @ inputs: let
-    config = import ./config; # import the module directly
-  in
-    flake-utils.lib.eachDefaultSystem (system: let
-      nixvimLib = nixvim.lib.${system};
-      pkgs = import nixpkgs {inherit system;};
-      nixvim' = nixvim.legacyPackages.${system};
-      nvim = nixvim'.makeNixvimWithModule {
-        inherit pkgs;
-        module = config;
-      };
-    in {
-      checks = {
-        # Run `nix flake check .` to verify that your config is not broken
-        default = nixvimLib.check.mkTestDerivationFromNvim {
-          inherit nvim;
-          name = "A nixvim configuration";
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        nixvimLib = inputs.nixvim.lib.${system};
+        nixvim' = inputs'.nixvim.legacyPackages;
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = import ./config;
+        };
+      in {
+        checks = {
+          # Run `nix flake check .` to verify that your config is not broken
+          default = nixvimLib.check.mkTestDerivationFromNvim {
+            inherit nvim;
+            name = "A nixvim configuration";
+          };
+        };
+
+        packages = {
+          # Lets you run `nix run .` to start nixvim
+          default = nvim;
         };
       };
-
-      packages = {
-        # Lets you run `nix run .` to start nixvim
-        default = nvim;
-      };
-    });
+    };
 }
