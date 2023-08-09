b5bd7cef83ca4308cd0b5c24a5c895337b75de84
Nick Hu <me@nickhu.co.uk>
2023-08-09 12:40:10 +0100

    treesitter-playground: init

--- config/default.nix
+++ config/default.nix
@@ -106,6 +106,7 @@
         nixvimInjections = true;
       };
       treesitter-context.enable = true;
+      treesitter-playground.enable = true;
       # These cmp plugins aren't be auto-enabled (no detection in extraConfigLuaPost)
       cmp-cmdline.enable = true;
       cmp-dap.enable = true;
