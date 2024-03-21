9efc30f74f28e479d0432b94132b4713e644c5e0
Nick Hu <me@nickhu.co.uk>
2024-03-21 22:04:34 +0000

    cmp: small fixes to mappings and snippet expansion

--- config/default.nix
+++ config/default.nix
@@ -600,47 +600,51 @@
           };
         };
         settings = {
-          mapping = {
-            "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-            "<C-f>" = "cmp.mapping.scroll_docs(4)";
-            "<CR>" = "cmp.mapping.confirm()";
-            "<Tab>" = ''
-              cmp.mapping(
-                function(fallback)
-                  local has_words_before = function()
-                    unpack = unpack or table.unpack
-                    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-                    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-                  end
-                  local luasnip = require("luasnip")
-                  if luasnip.expand_or_locally_jumpable() then
-                    luasnip.expand_or_jump()
-                  elseif cmp.visible() then
-                    cmp.select_next_item()
-                  elseif has_words_before() then
-                    cmp.complete()
-                  else
-                    fallback()
-                  end
-                end,
-                { "i", "s" })
-            '';
-            "<S-Tab>" = ''
-              cmp.mapping(
-                function(fallback)
-                  local luasnip = require("luasnip")
-                  if luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                  elseif cmp.visible() then
-                    cmp.select_prev_item()
-                  else
-                    fallback()
-                  end
-                end,
-                { "i", "s" })
-            '';
-          };
-          snippet.expand = "luasnip";
+          mapping = helpers.mkRaw ''
+            cmp.mapping.preset.insert({
+              ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+              ['<C-f>'] = cmp.mapping.scroll_docs(4),
+              ['<CR>'] = cmp.mapping.confirm(),
+              ['<Tab>'] =
+                cmp.mapping(
+                  function(fallback)
+                    local has_words_before = function()
+                      unpack = unpack or table.unpack
+                      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+                    end
+                    local luasnip = require("luasnip")
+                    if luasnip.expand_or_locally_jumpable() then
+                      luasnip.expand_or_jump()
+                    elseif cmp.visible() then
+                      cmp.select_next_item()
+                    elseif has_words_before() then
+                      cmp.complete()
+                    else
+                      fallback()
+                    end
+                  end,
+                  { "i", "s" }),
+              ['<S-Tab>'] =
+                cmp.mapping(
+                  function(fallback)
+                    local luasnip = require("luasnip")
+                    if luasnip.jumpable(-1) then
+                      luasnip.jump(-1)
+                    elseif cmp.visible() then
+                      cmp.select_prev_item()
+                    else
+                      fallback()
+                    end
+                  end,
+                  { "i", "s" }),
+            })
+          '';
+          snippet.expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
           sources = [
             {
               name = "nvim_lsp_signature_help";
