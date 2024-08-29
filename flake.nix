c78929a20fd70ebe945fa974e2e23bf7860b706e
Nick Hu <me@nickhu.co.uk>
2024-08-29 16:40:35 +0100

    forester.nvim: switch back to upstream

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       flake = false;
     };
     forester-nvim = {
-      url = "github:NickHu/forester.nvim";
+      url = "github:kentookura/forester.nvim";
       flake = false;
     };
     lsp-progress-nvim = {
